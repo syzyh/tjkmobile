ba0a56bc533e9984d3cd4473516200f1f10a0247
syzyh <13521913746@163.com>
2018-05-03 14:39:13 +0800

    05.038

--- src/pages/discussion/reply.jsx
+++ src/pages/discussion/reply.jsx
@@ -12,6 +12,7 @@ class Reply extends Component {
     if (!this.props.data || this.props.data.length === 0) return null;
     const hasHidenReply = this.props.data.length > this.state.limt;
     const showData = this.props.data.slice(0, this.state.limit);
+    console.log(hasHidenReply);
     return (
       <div className={style['reply']}>
         <div className={style['reply-arrow']}></div>
@@ -40,9 +41,11 @@ class Reply extends Component {
             </div>
             ))
           }
-          {
-            hasHidenReply ? <div className={style['reply-showmore']} onClick={this.setState({limit: this.state.limit + 5})}>查看更多</div> : null
-          }
+          <div>
+            {
+              hasHidenReply ? <div className={style['reply-showmore']} onClick={this.setState({limit: this.state.limit + 5})}>查看更多</div> : null
+            }
+          </div>
         </div>
       </div>
     );
